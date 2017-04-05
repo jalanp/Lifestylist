eb4f86fcfa2e6e2ffa17336f99f7791a353e842c
Akram Zaky <akramzaky@Akrams-MacBook-Pro.local>
2017-04-05 13:15:11 -0400

    Varibales Decleared

--- app/src/main/java/com/a3a04/android/lifestylist/main/SettingsActivity.java
+++ app/src/main/java/com/a3a04/android/lifestylist/main/SettingsActivity.java
@@ -4,12 +4,19 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.ToggleButton;
 
 import com.a3a04.android.lifestylist.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
     ActionBar mActionBar;
+    EditText input1,input2,input3,input4,input5;
+    ToggleButton sleep,meal,workout;
+    SeekBar Fitbit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,15 @@ public class SettingsActivity extends AppCompatActivity {
         mActionBar.setDisplayHomeAsUpEnabled(true);
         mActionBar.show();
 
+        input1=(EditText) findViewById(R.id.input1);
+        input2=(EditText) findViewById(R.id.input2);
+        input3=(EditText) findViewById(R.id.input3);
+        input4=(EditText) findViewById(R.id.input4);
+        input5=(EditText) findViewById(R.id.input5);
+        sleep=(ToggleButton) findViewById(R.id.SleepToggle);
+        meal=(ToggleButton) findViewById(R.id.MealToggle;
+        workout=(ToggleButton) findViewById(R.id.WorkoutToggle);
+        Fitbit=(SeekBar) findViewById(R.id.Fitbit);
 
 
     }
